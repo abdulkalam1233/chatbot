33e922a94f1cb193c916fa7da5c5b32ac2178266
Abdul Kalam <abdul@incubyte.co>
2025-09-28 19:10:21 +0530

    ENH: setup web

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,8 @@
+// This file will serve as the main entry point for the UI package
+// Add more exports here as you create components
+// Export utilities
+
+// Export components
+export * from "./components/button";
+export * from "./components/input";
+export * from "./lib/utils";
